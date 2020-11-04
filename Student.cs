bef68487e032cc79f90051bf8301644f7096d5a0
Ciro Andres Cobos Sanchez <cacoboss@unal.edu.co>
2020-11-04 09:24:09 -0500

    Initial Commit: Template of the solution

---
+++ Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SoluciónProfe
+{
+    public class Student
+    {
+        protected uint _studentID;
+        protected string _email;
+
+        private Asignements _listOfAsignements;
+    }
+}
